62687f2ea4da474838318f6104adaa7fe5b98552
ExpIMD <fourexpimd@gmail.com>
2025-07-14 10:54:11 +0300

    Improved the structure of .h files

--- avl_binary_search_tree.h
+++ avl_binary_search_tree.h
@@ -3,56 +3,57 @@
 
 #include <stdexcept>
 
-template<typename Key, typename Compare = std::less<Key>, typename Alloc = std::allocator<Key>>
-class avl_binary_search_tree{
-private:
-    class node{
-    public:
-        Key _key;
+namespace IMD{
 
-        node* _left;
-        node* _right;
-
-        size_t _height;
-
-        
-        
-        node(const Key& key, node* left = NULL, node* right = NULL, size_t height = 1) : _key(key), _left(left), _right(right), _height(height) {this->update_height();}
-        
-        bool operator==(const node& other){
-            return this->_height == 0 && other._height == 0 ? true : this->_key == other._key;
-        }
-
-        size_t get_height() const noexcept {
-            return this->_height;
-        }
-
-        size_t get_balance() const noexcept{
-            size_t left_height{0};
-            size_t right_height{0};
+    template<typename Key, typename Compare = std::less<Key>, typename Alloc = std::allocator<Key>>
+    class avl_binary_search_tree{
+    private:
+        class node{
+        public:
+            Key _key;
+    
+            node* _left;
+            node* _right;
+    
+            size_t _height;
+    
             
-            if (this->_left != NULL)
-                left_height = this->_left->get_height();
-            if (this->_right != NULL)
-                right_height = this->_right->get_height();
             
-            return std::abs(left_height - right_height);
-        }
-
-        void update_height() noexcept {
-            size_t left_height{0};
-            size_t right_height{0};
+            node(const Key& key, node* left = NULL, node* right = NULL, size_t height = 1) : _key(key), _left(left), _right(right), _height(height) {this->update_height();}
             
-            if (this->_left != NULL)
-                left_height = this->_left->get_height();
-            if (this->_right != NULL)
-                right_height = this->_right->get_height();
-            
-            this->_height = 1 + std::max(left_height, right_height);
-        }
-    };
-
+            bool operator==(const node& other){
+                return this->_height == 0 && other._height == 0 ? true : this->_key == other._key;
+            }
+    
+            size_t get_height() const noexcept {
+                return this->_height;
+            }
+    
+            size_t get_balance() const noexcept{
+                size_t left_height{0};
+                size_t right_height{0};
+                
+                if (this->_left != NULL)
+                    left_height = this->_left->get_height();
+                if (this->_right != NULL)
+                    right_height = this->_right->get_height();
+                
+                return left_height - right_height;
+            }
+    
+            void update_height() noexcept {
+                size_t left_height{0};
+                size_t right_height{0};
+                
+                if (this->_left != NULL)
+                    left_height = this->_left->get_height();
+                if (this->_right != NULL)
+                    right_height = this->_right->get_height();
+                
+                this->_height = 1 + std::max(left_height, right_height);
+            }
+        };   
+    };   
 }
-
 #endif
 
